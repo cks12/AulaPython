007812cb861406ba986ee528b53f27c7ad2dfff2
Caio Fresneda <caiofresneda@gmail.com>
2022-05-27 10:23:59 -0300

    aaaa

--- Aula09/db.py
+++ Aula09/db.py
@@ -1,21 +1,11 @@
 import sqlite3
 from sqlite3 import Error
-class Database:
-    def __init__(self):
-        self.conn = None
-    def createConnection(self,db_file):
-        '''Creates new connection with database'''
-        try: 
-            self.conn = sqlite3.connect(db_file)
-            print(sqlite3.version)
 
-        except Error as err:
-            print(err)
-
-    def __list_to_string__ (self, elemnt: list):
+class utils:
+    def __list_to_string__ (self, element: list):
         newString = ""
-        for e in newElements:
-            string = f"{e} "
+        for i,e in enumerate(element):
+            string = f"{e} , " if i != (len(element) - 1) else f"{e}"
             newString += string
         return newString
 
@@ -25,33 +15,82 @@ class Database:
                 string = "{item} {type}".format(item = e[0], type = e[1])
                 newElements.append(string)
             return newElements
-            
-    def create_table(self, name: str, elements: list) -> None:
+
+class Database(utils):
+    def __init__(self):
+        self.conn = None
+        super().__init__()
+
+    def create_connection(self,db_file) -> None:
+        '''Creates new connection with database'''
+        try: 
+            self.conn = sqlite3.connect(db_file)
+            print(sqlite3.version)
+
+        except Error as err:
+            print(err)
+
+    def __create_table__(self, name: str, elements: list) -> None:
         ''' Create table with name and elements ([[item ,type]]) '''
         try:
             cur = self.conn.cursor()
-            newArr = __item_type__(elements)
+            newArr = self.__item_type__(elements)
             newString = self.__list_to_string__(newArr)
+            print(newString)
             cur.execute(f'''CREATE TABLE {name} ({newString})''')
             self.conn.commit()
         except Error as err:
             print(err)
 
-    def insertInTable(self, name: str, elements:list):
+    def __insert_in_table__ (self, name: str, elements:list) -> None:
         try:
-            newString = self.__list_to_string__(newArr)
+            newString = self.__list_to_string__(elements)
             cur = self.conn.cursor()
             cur.execute(f"INSERT INTO {name} VALUES ({newString})")
             self.conn.commit()
         except Error as err:
             print(err)
+
+    def __get_all__ (self, name):
+        try:
+            cur = self.conn.cursor()
+            rows = cur.execute(f"SELECT * FROM {name}")
+            for i in rows:
+                print(i)
+        
+        except Error as err:
+            print(err)     
     def get_conn (self):
         return self.conn
 
-db = Database()
-db.createConnection(r"databse.db")
 
-db.insertInTable("sla",
-   [ "caio",
-    "caiofresneda@gmail.com",10,]
-)
+class orm_db(Database):
+    def __init__(self):
+        super().__init__()
+    def createModal(self, elements: list):
+        try:
+            arr = []
+            for element in elements:
+                newArr = [element["name"], element["type"]]
+                arr.append(newArr)
+        except:
+            pass
+
+
+db = orm_db()
+db.create_connection(r"databse.db")
+
+# db.create_table("peoples", [
+#     ["name","text"],
+#     ["email","text"],
+#     ["age","real"],
+# ])
+
+# db.insertInTable("peoples", [
+#     "'caio Caik Fresneda de souza'",
+#     "'caiodesouza12@gmail.com'",
+#     "'21'"
+# ])
+
+users = db.get_all("peoples")
+print(users)
---
+++ acs/logica/ac02/quest03.py
@@ -0,0 +1,7 @@
+soma,cont = 0 , 1
+
+while cont <= 5:
+    num = int(input())
+    soma = soma + num
+    cont = cont + 1
+print(f"A soma dos numeros é: {soma}")
