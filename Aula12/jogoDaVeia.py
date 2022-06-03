18a72e8035df75c9aca436b6a9a8d96069cc8a10
Caio Fresneda <caiofresneda@gmail.com>
2022-06-03 01:40:29 -0300

    sla

--- Aula12/jogoDaVeia.py
+++ Aula12/jogoDaVeia.py
@@ -1,34 +1,59 @@
-import os
-class JogoDaVelha:
+from mimetypes import init
+import os, time
+
+class verfy:
+    def __init__(self, matriz, size) -> None:
+        self.matriz = matriz
+        self.size = size
+
+    def __verfy__(self, arr: list):
+        for i in range(len(arr)):
+            __fristElement__ = arr[i][0]
+            __tempArr__ = []
+            for element in arr[i]:
+                __tempArr__.append((__fristElement__ == element))
+            listVerfy = sum(__tempArr__)
+            print(listVerfy, len(arr[i]))
+            if(listVerfy == len(self.matriz[i])):
+                return True, __fristElement__
+        return False, ""
+
+    def verfy(self):
+        colum = []
+        line = []
+        for lineIndex in range(self.size):
+            __tempColum__ = []
+            __tempLine__ = []
+            for columIndex in range(self.size):
+                __colum__ = self.matriz[columIndex][lineIndex]
+                __line__ = self.matriz[lineIndex][columIndex]
+                __tempLine__.append(__line__)
+                __tempColum__.append(__colum__)
+            colum.append(__tempColum__)
+            line.append(__tempLine__)
+            return self.__verfy__(__tempColum__) or self.verfy(__tempLine__)
+
+class JogoDaVelha(verfy):
     def __init__(self, size):
         self.matriz = []
         self.turn = 0
         self.size = size
-
+        super().__init__(self.matriz, self.size)
     def createGame(self):
-
         __letters__ = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] 
-        
         for lines in range(self.size):
-
             letter = __letters__[lines]
             letter = letter.upper()
             temp = []
-
             for i in range(self.size):
-
                 temp.append(f"{letter}{i}")
-            
             self.matriz.append(temp)
-
     def clear(self):
         os.system("cls" if os.name=="nt" else "clear")
 
     def printGame(self):
-
         self.clear()
         print("Turno:",self.turn)
-
         for line in range(3):
             l = self.matriz[line]
             print(*l)
@@ -44,31 +69,30 @@ class JogoDaVelha:
             self.turn += 1
         except:
             print("> Por favor digite uma casa livre ou valida")
+            time.sleep(2)
     
     def vitory(self):
-        v = False
-        col = self.matriz[0][0]
-        for line in range(self.size):
-            if self.matriz[line][0] == self.matriz[line][1] and self.matriz[line][0] == self.matriz[line][2]:
-                return not v
-            for coluns in range(self.size):
-                if(self.matriz[line][coluns] != col):
-                    break
-                coluns
-        return v
+        return self.verfy()
 
+class Game(JogoDaVelha):
+    def __init__(self, playes) -> None:
+        self.playes = playes
+        super().__init__(3)
+        self.createGame()
+    def start(self):
+        while True:
+            victory = self.vitory()
+            if victory[0]:
+                print("> O vencedor foi....")
+                print(victory[1])
+                break
+            for player in self.playes:
+                self.printGame()
+                print(victory)
+                user = input(f"> Digite uma casa para trocar por {player}: ")
+                self.change(user, player)
+                
 if __name__ == "__main__":
+    game = Game(["X","O"])
+    game.start()
     print("> Jogo da velha iniciado !!!")
-    JogoDaVelha = JogoDaVelha(3)
-    JogoDaVelha.createGame()
-    JogoDaVelha.printGame()
-    while True:
-        if JogoDaVelha.vitory():
-            print("Parabén, vc venceu!")
-            break
-        user1 = input("> Digite uma casa para trocar por X: ")
-        JogoDaVelha.change(user1, "X")
-        JogoDaVelha.printGame()
-        user2 = input("> Digite uma casa para trocar por O: ")
-        JogoDaVelha.change(user2, "O")
-        JogoDaVelha.printGame()
