4b9042eb8b011a135e9a0216718a1fd9943939bd
cks12 <caiofresneda@gmail.com>
2023-03-08 22:20:49 +0000

    remove old files

--- acs/linguagens/ac02/quest 2.py
+++
@@ -1,7 +0,0 @@
-alf = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-i = int(input())
-
-if 1 <= i <= 26:
-    for c in range(i):
-        s = f'{alf[c]}'*int(c + 1)
-        print(s)
--- acs/logica/ac02/quest02.py
+++
@@ -1,7 +0,0 @@
-cont,idade,peso = 0,0,0
-
-while peso < 5:
-    if peso>5:
-        cont=cont+idade
-    idade=idade+3
-    idade = int(input(""))
