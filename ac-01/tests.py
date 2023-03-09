6b136adca32d94c0722519f328f84582a7ad568c
cks12 <caiofresneda@gmail.com>
2023-03-09 22:37:10 +0000

    Ac 01 python Finalizada

--- ac-01/tests.py
+++ ac-01/tests.py
@@ -18,6 +18,25 @@ class TestSomatorio(unittest.TestCase):
         self.assertEqual(potencia_de_2(3), False)
         self.assertEqual(potencia_de_2(10), False)
         self.assertEqual(potencia_de_2(1023), False)
+    
+    def test_qtd_digitos(self):
+        self.assertEqual(qtd_digitos(4), 1)
+        self.assertEqual(qtd_digitos(9),1)
+        self.assertEqual(qtd_digitos(10), 2)
+        self.assertEqual(qtd_digitos(99), 2)
+        self.assertEqual(qtd_digitos(1071), 4)
+    
+    def test_soma_digitos(self):
+        self.assertEqual(soma_digitos(4), 4)
+        self.assertEqual(soma_digitos(45), 9)
+        self.assertEqual(soma_digitos(451), 10)
+        self.assertEqual(soma_digitos(2345), 14)
+    
+    def test_soma_lista(self):
+        self.assertEqual(soma_lista([1,2,3,4],0), 10)
+        self.assertEqual(soma_lista([1,2,3,4],2), 7)
+        self.assertEqual(soma_lista([22,33,44,55],1), 132)
+
 
 if __name__ == '__main__':
     unittest.main()
